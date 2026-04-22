1149aa23d775abf685aed9cce6123490b79d687c
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-04-22 19:02:41 +0530

    Update 2a) JWT Part 1.java

--- 2a) JWT Part 1.java
+++ 2a) JWT Part 1.java
@@ -49,3 +49,55 @@ public interface RoleRepository extends JpaRepository<Role,Long>{
 	Optional<Role> findByName(String name);
 }
 
+//dto
+//AuthResponse.java
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+	private String token;
+	private String email;
+	private String name;
+	private String role;
+}
+//LoginRequest.java
+package dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+	@Email
+	@NotBlank
+	private String email;
+	
+	@NotBlank
+	private String password;
+}
+//RegisterRequest.java
+package dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+	@NotBlank(message = "Name is required")
+	private String Name;
+	
+	@Email(message = "Enter a valid email")
+	@NotBlank(message = "Email is required")
+	private String email;
+	
+	@Size(min=6, message="Password must be atleast 6 characters")
+	@NotBlank(message = "Password is required")
+	private String password;
+}
