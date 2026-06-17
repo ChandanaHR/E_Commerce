bb83788b823f249658e99a7957b93be15929d54c
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-17 18:59:33 +0530

    Update 2_security_config.java

--- 2_security_config.java
+++ 2_security_config.java
@@ -53,3 +53,83 @@ public class JwtUtil {
 }
 
 //CustomerUserDetailsService.java
+package security;
+
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import Spring_and_SpringBoot.Entities.User;
+import lombok.RequiredArgsConstructor;
+import repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerUserDetailsService implements UserDetailsService {
+	private final UserRepository userRepository = null;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email) 
+				    .orElseThrow(()->new UsernameNotFoundException("User not found" +email));
+		
+		var authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName()))
+				.collect(Collectors.toList());
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getEmail(),
+				user.getPassword(),
+				authorities);
+	}
+}
+
+
+//JwtAuthfilter.java
+package security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+	private JwtUtil jwtUtil;
+	private CustomerUserDetailsService userDetailsService;
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String authHeader = request.getHeader("Authorization");
+		
+		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		String token = authHeader.substring(7);
+		String email = jwtUtil.extractEmail(token);
+		
+		if(email!=null && SecurityContextHolder.getContext().getAuthentication()==null) {
+			UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+			if(jwtUtil.isTokenValid(token, userDetails.getUsername())) {
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
